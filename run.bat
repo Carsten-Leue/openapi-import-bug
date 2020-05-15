bf869f94ec5ca12361e76ba43d515ae36e9d9015
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-05-15 15:05:31 +0200

    initial checkin

---
+++ run.bat
@@ -0,0 +1,2 @@
+java -jar openapi-generator-cli-4.3.0.jar generate -g openapi -i openapi/index.json -o out/4.3.0/
+java -jar openapi-generator-cli-4.3.1.jar generate -g openapi -i openapi/index.json -o out/4.3.1/
